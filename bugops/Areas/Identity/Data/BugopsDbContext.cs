60f906b57e71969c30aa250e432f5c00216bb262
Dwarf1er <SerolTech@protonmail.com>
2023-05-13 14:08:09 -0400

    Added basic user register/login/logout

---
+++ bugops/Areas/Identity/Data/BugopsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using bugops.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace bugops.Areas.Identity.Data;
+
+public class BugopsDbContext : IdentityDbContext<BugopsUser>
+{
+    public BugopsDbContext(DbContextOptions<BugopsDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new BugopsUserEntityConfiguration());
+    }
+}
+
+public class BugopsUserEntityConfiguration: IEntityTypeConfiguration<BugopsUser> {
+    public void Configure(EntityTypeBuilder<BugopsUser> builder) {
+        builder.Property(x => x.FirstName).HasMaxLength(255);
+        builder.Property(x => x.LastName).HasMaxLength(255);
+    }
+}
--- bugops/Program.cs
+++ bugops/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using bugops.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BugopsDbContextConnection") ?? throw new InvalidOperationException("Connection string 'BugopsDbContextConnection' not found.");
+
+builder.Services.AddDbContext<BugopsDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<BugopsUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<BugopsDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -23,5 +31,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
